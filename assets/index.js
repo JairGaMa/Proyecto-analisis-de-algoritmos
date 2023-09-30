9e40340dbade55d2c89ad7073565eaa7e510ef02
JairGaMa <jair191103@gmail.com>
2023-09-30 13:42:26 -0600

    modified:   assets/index.js

--- assets/index.js
+++ assets/index.js
@@ -19,24 +19,4 @@ function integrantes() {
     `,
     html: '<p>Camacho García Julia Guadalupe<br>Garcia Mayorga Brayan Jair<br>Ramirez Vazquez Wendy Itzel</p>'
   })
-function pageLoaded() {
-    let loaderSection = document.querySelector('.loader-section');       loaderSection.classList.add('loaded');
-  }
-  document.onload = pageLoaded();
-/*
-function ordenamiento() {
-    contenido=document.getElementById("contenido")
-    contenido.innerHTML="Hola, mundo";
-}
-*/
-function integrantes() {
-  Swal.fire({
-    title: 'Integrantes del equipo    5EJ',
-    color: 'black',
-    background: '#fff',
-    backdrop: `
-      rgba(0,0,123,0.4)
-    `,
-    html: '<p>Camacho García Julia Guadalupe<br>Garcia Mayorga Brayan Jair<br>Ramirez Vazquez Wendy Itzel</p>'
-  })
 }
