ed5aeedc01038b6aa942270fc9356131e88b18fb
JairGaMa <74119858+JairGaMa@users.noreply.github.com>
2023-09-30 11:50:10 -0600

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+function pageLoaded() {
+    let loaderSection = document.querySelector('.loader-section');       loaderSection.classList.add('loaded');
+  }
+  document.onload = pageLoaded();
+/*
+function ordenamiento() {
+    contenido=document.getElementById("contenido")
+    contenido.innerHTML="Hola, mundo";
+}
+*/
+function integrantes() {
+  Swal.fire({
+    title: 'Integrantes del equipo    5EJ',
+    color: 'black',
+    background: '#fff',
+    backdrop: `
+      rgba(0,0,123,0.4)
+    `,
+    html: '<p>Camacho García Julia Guadalupe<br>Garcia Mayorga Brayan Jair<br>Ramirez Vazquez Wendy Itzel</p>'
+  })
+}
